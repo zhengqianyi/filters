8a39f568084e5e44eb5e6abd87adb47d3e56c2d0
zhengqianyi <1424365539@qq.com>
2021-09-14 20:58:15 +0800

    "基础测试完成"

--- ft/filter.hpp
+++ ft/filter.hpp
@@ -1,42 +1,35 @@
 #ifndef FT_FILTER_HPP
 #define FT_FILTER_HPP
 
-#include <ft/wrap.hpp>
-
-namespace ft{
-
-class filter
+#include <string>
+namespace ft
 {
-private:
-    /* data */
-public:
-    filter() = default;
-    virtual ~filter();
 
-    /// Adds an element to the Bloom filter.
-    template <typename T>
-    void add(T const& x)
+    class filter
     {
-        add(wrap(x));
-    }
-    /// Adds an element to the Bloom filter.
-    virtual void add(object const& o) = 0;
+    private:
+        /* data */
+    public:
 
-    /// Retrieves the count of an element.
-    template <typename T>
-    bool lookup(T const& x) const
-    {
-        return lookup(wrap(x));
-    }
-    /// Retrieves the count of an element.
-    virtual bool lookup(object const& o) const = 0;
+        /// Adds an element to the Bloom filter.
+        template <typename T>
+        void add(T const &x)
+        {
+            add(wrap(x));
+        }
+        /// Adds an element to the Bloom filter.
+        virtual void add(std::string const &o) = 0;
 
-
-};
-
+        /// Retrieves the count of an element.
+        template <typename T>
+        bool lookup(T const &x) const
+        {
+            return lookup(wrap(x));
+        }
+        /// Retrieves the count of an element.
+        virtual bool lookup(std::string const &o) const = 0;
+    };
 
 } // namespace ft
-
-
 
 #endif
